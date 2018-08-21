6da642668a74b12c32dad090ffa57fa040f99a28
josephj <josephj6802@gmail.com>
2018-08-22 00:14:40 +1000

    Use process.env.PORT

--- app.js
+++ app.js
@@ -70,4 +70,4 @@ app.get("/error-async-await", async (req, res, next) => {
   console.log(data);
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log("Example app listening on port 3000!"));
